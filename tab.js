e9dc1a84710e0390174ccc9c409f635a4cae4d89
Your Name <you@example.com>
2021-09-20 15:21:54 +0300

    fitst commit

---
+++ tab.js
@@ -0,0 +1,27 @@
+ const tabsBtm = document.querySelectorAll('.tabs__nav-btn');
+const tabsItems = document.querySelectorAll('.tabs__item');
+
+tabsBtm.forEach(onTabClick);
+
+function onTabClick(item){
+    item.addEventListener('click', function (){
+        let currentBtm = item;
+        let tabId = currentBtm.getAttribute('data-tab');
+        let currentTab = document.querySelector(tabId);
+
+
+        if (! currentBtm.classList.contains('active')){
+            tabsBtm.forEach(function (item){
+                item.classList.remove('active');
+            });
+
+            tabsItems.forEach(function (item){
+                item.classList.remove('active');
+            });
+            currentBtm.classList.add('active');
+            currentTab.classList.add('active');
+        };
+    });
+}
+
+document.querySelector('.tabs__nav-btn').click();
